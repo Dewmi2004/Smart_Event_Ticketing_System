b3b5744889388e893ebb27a428ceaddbb988f101
Dewmi2004 <imashadewmi557@gmail.com>
2026-03-03 15:52:54 +0530

    Start Back_End development , Dependencies OK

---
+++ Event_Ticketing-Back_End/src/main/java/lk/ijse/event_ticketingback_end/controller/EventController.java
@@ -0,0 +1,4 @@
+package lk.ijse.event_ticketingback_end.controller;
+
+public class EventController {
+}
---
+++ Event_Ticketing-Back_End/src/test/java/lk/ijse/event_ticketingback_end/EventTicketingBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.event_ticketingback_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
